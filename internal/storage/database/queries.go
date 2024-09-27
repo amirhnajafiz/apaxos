151284b49ce76f4f19e1a4ae2033fa0b350b790e
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-27 16:17:20 -0400

    add: block model

--- internal/storage/database/queries.go
+++ internal/storage/database/queries.go
@@ -6,12 +6,12 @@ import (
 	"github.com/f24-cse535/apaxos/pkg/model"
 )
 
-func (d Database) InsertTransactions(transactions []*model.Transaction) error {
+func (d Database) InsertTransactions(transactions []model.Transaction) error {
 	return d.db.CreateInBatches(transactions, len(transactions)).Error
 }
 
-func (d Database) GetTransactions() ([]*model.Transaction, error) {
-	var transactions []*model.Transaction
+func (d Database) GetTransactions() ([]model.Transaction, error) {
+	var transactions []model.Transaction
 
 	if err := d.db.Model(&model.Transaction{}).Find(transactions).Error; err != nil {
 		return nil, fmt.Errorf("failed to query transactions: %v", err)
