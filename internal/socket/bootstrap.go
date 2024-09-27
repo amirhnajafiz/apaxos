708cb5628526197726d7bcfd0a38573da2505133
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-27 12:55:46 -0400

    init: socket and model

---
+++ internal/socket/bootstrap.go
@@ -0,0 +1,6 @@
+package socket
+
+// Keep a map of the nodes and their sockets
+// Use a channel to communicate with http handler
+// Accept new transactions (a block of transactions)
+// Implement APAXOS for transactions.
