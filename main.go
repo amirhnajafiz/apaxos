9b27b143f40ae9e766b2c70c5a34c83d009c74f0
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-27 12:58:23 -0400

    add: bootstrap methods in main

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/f24-cse535/apaxos/internal/config"
+	"github.com/f24-cse535/apaxos/internal/http"
+)
 
+func main() {
+	// load configs
+	cfg := config.New("config.yaml")
+
+	// TODO: open db connection
+	// TODO: open redis connection
+
+	// bootstrap http server as a goroutine
+	go http.Bootstrap(cfg.HTTP)
+
+	// bootstrap socket interface
 }
