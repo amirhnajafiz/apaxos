a1cd230c8e664e0963fd17bbc5e06d70805b4988
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-27 12:14:46 -0400

    add: koanf config loader

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,65 @@
 package config
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+	"github.com/knadh/koanf/v2"
+	"github.com/tidwall/pretty"
+)
+
+// Prefix indicates environment variables prefix.
+const Prefix = "apax_"
+
+// Config holds all configurations.
+type Config struct {
+}
+
+// New reads configuration with koanf.
+func New(path string) Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from file
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider(path), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	// load environment variables
+	if err := k.Load(env.Provider(Prefix, ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(
+			strings.TrimPrefix(s, Prefix)), "__", ".")
+	}), nil); err != nil {
+		log.Printf("error loading environment variables: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	indent, err := json.MarshalIndent(instance, "", "\t")
+	if err != nil {
+		log.Fatalf("error marshaling config to json: %s", err)
+	}
+
+	indent = pretty.Color(indent, nil)
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	=============================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
+}
--- internal/config/default.go
+++ internal/config/default.go
@@ -1,1 +1,6 @@
 package config
+
+// Default return default configuration.
+func Default() Config {
+	return Config{}
+}
