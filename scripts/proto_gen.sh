02e6ea24c6fbd6064129e2fe199a3674c3e85ff0
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-12 16:07:49 -0400

    update: proto pkg

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# setting env variables which are used
+# setting variables which are used
 # as protoc input arguments.
-export $SRC_DIR=proto/
-export $DST_DIR=pkg/
-export $PROTO=transactions
+SRC_DIR="proto/"
+DST_DIR="pkg/"
+PROTO="transactions"
 
 # make sure to have protoc installed on your system
 protoc -I=$SRC_DIR --go_out=$DST_DIR $SRC_DIR/$PROTO.proto || echo "protoc not installed on this machine!"
