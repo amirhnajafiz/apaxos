ad16534ab7cc8c7915ac4987924fb53503a63d36
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-12 16:34:25 -0400

    feat: local memory

--- internal/storage/local/memory.go
+++ internal/storage/local/memory.go
@@ -1,7 +1,22 @@
 package local
 
-type Memory struct{}
+import "github.com/f24-cse535/apaxos/pkg/models"
 
+// Memory is a local storage that is used
+// for each node.
+type Memory struct {
+	sequenceNumber int64
+	ballotNumber   *models.BallotNumber
+	acceptedNum    *models.BallotNumber
+	acceptedVal    []*models.Block
+	datastore      []*models.Transaction
+}
+
+// NewMemory returns an instance of the memory struct.
 func NewMemory() *Memory {
-	return &Memory{}
+	return &Memory{
+		sequenceNumber: 0,
+		acceptedVal:    make([]*models.Block, 0),
+		datastore:      make([]*models.Transaction, 0),
+	}
 }
---
+++ internal/storage/local/operations.go
@@ -0,0 +1,52 @@
+package local
+
+import "github.com/f24-cse535/apaxos/pkg/models"
+
+// Memory operations for sequence_number.
+func (m *Memory) GetSequenceNumber() int64 {
+	return m.sequenceNumber
+}
+
+func (m *Memory) IncSequenceNumber() {
+	m.sequenceNumber++
+}
+
+// Memory operations for ballot_number.
+func (m *Memory) SetBallotNumber(instance *models.BallotNumber) {
+	m.ballotNumber = instance
+}
+
+func (m *Memory) GetBallotNumber() *models.BallotNumber {
+	return m.ballotNumber
+}
+
+// Memory operations for accepted_num.
+func (m *Memory) SetAcceptedNum(instance *models.BallotNumber) {
+	m.acceptedNum = instance
+}
+
+func (m *Memory) GetAcceptedNum() *models.BallotNumber {
+	return m.acceptedNum
+}
+
+// Memory operations for accepted_val.
+func (m *Memory) SetAcceptedVal(instance []*models.Block) {
+	m.acceptedVal = instance
+}
+
+func (m *Memory) GetAcceptedVal() []*models.Block {
+	return m.acceptedVal
+}
+
+// Memory operations for datastore.
+func (m *Memory) AddTransactionToDatastore(instance *models.Transaction) {
+	m.datastore = append(m.datastore, instance)
+}
+
+func (m *Memory) GetDatastore() []*models.Transaction {
+	return m.datastore
+}
+
+func (m *Memory) ResetDatastore() {
+	m.datastore = make([]*models.Transaction, 0)
+}
