3e589d0f0fc7a04f11cabf8534a69f5f8b56366a
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-12 23:42:17 -0400

    init: client transactions

--- internal/grpc/client/transactions.go
+++ internal/grpc/client/transactions.go
@@ -1,6 +1,25 @@
 package client
 
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+)
+
+// TransactionsDialer is used to call RPCs for transactions service.
 type TransactionsDialer struct{}
+
+// connect should be called in the beginning of each method to establish a connection.
+func (t *TransactionsDialer) connect(address string) (*grpc.ClientConn, error) {
+	var opts []grpc.DialOption
+
+	conn, err := grpc.NewClient(address, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("[grpc/client/transactionsdialer] failed to make rpc call: %v", err)
+	}
+
+	return conn, nil
+}
 
 func (t *TransactionsDialer) NewTransaction() {}
 
