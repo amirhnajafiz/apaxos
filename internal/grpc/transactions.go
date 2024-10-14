c405fd489d63faf9172f44775888cb290aee69c3
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-14 16:01:20 -0400

    add: rpc log trace for transactions

--- internal/grpc/transactions.go
+++ internal/grpc/transactions.go
@@ -27,6 +27,9 @@ type transactionsServer struct {
 // NewTransaction is called for registering a new transaction.
 // The handler sends a demand to consensus and waits for a response.
 func (s *transactionsServer) NewTransaction(ctx context.Context, req *apaxos.Transaction) (*transactions.TransactionResponse, error) {
+	s.Logger.Debug("rpc called NewTransaction")
+
+	// create a response instance
 	response := transactions.TransactionResponse{Result: false}
 
 	// send a message to consensus to process a transaction
@@ -53,6 +56,8 @@ func (s *transactionsServer) NewTransaction(ctx context.Context, req *apaxos.Tra
 
 // PrintBalance is a simple operation that reads the client balance from node's memory.
 func (s *transactionsServer) PrintBalance(ctx context.Context, req *transactions.PrintBalanceRequest) (*transactions.PrintBalanceResponse, error) {
+	s.Logger.Debug("rpc called PrintBalance")
+
 	return &transactions.PrintBalanceResponse{
 		Balance: s.Memory.GetBalance(req.Client),
 	}, nil
@@ -60,12 +65,14 @@ func (s *transactionsServer) PrintBalance(ctx context.Context, req *transactions
 
 // PrintLogs returns the node datastore and accepted val.
 func (s *transactionsServer) PrintLogs(req *emptypb.Empty, stream transactions.Transactions_PrintLogsServer) error {
+	s.Logger.Debug("rpc called PrintLogs")
+
 	// first send the datastore block
 	if err := stream.Send(s.Memory.GetDatastore().ToProtoModel()); err != nil {
 		return err
 	}
 
-	// send accepted vau blocks
+	// send accepted_val blocks
 	for _, block := range s.Memory.GetAcceptedVal() {
 		if err := stream.Send(block.ToProtoModel()); err != nil {
 			return err
@@ -77,22 +84,28 @@ func (s *transactionsServer) PrintLogs(req *emptypb.Empty, stream transactions.T
 
 // PrintDB get's blocks from MongoDB and sends them as proto blocks.
 func (s *transactionsServer) PrintDB(req *emptypb.Empty, stream transactions.Transactions_PrintDBServer) error {
+	s.Logger.Debug("rpc called PrintDB")
+
+	// get all blocks from MongoDB
 	blocks, err := s.Database.GetBlocks()
 	if err != nil {
 		return err
 	}
 
+	// send them one by one
 	for _, block := range blocks {
 		if err := stream.Send(block.ToProtoModel()); err != nil {
 			return err
 		}
 	}
 
-	return nil
+	return stream.RecvMsg(nil)
 }
 
 // Performance function returns the node's throughput and latency.
 func (s *transactionsServer) Performance(ctx context.Context, req *emptypb.Empty) (*transactions.PerformanceResponse, error) {
+	s.Logger.Debug("rpc called Performance")
+
 	return &transactions.PerformanceResponse{
 		Throughput: 1000.5,
 		Latency:    20.3,
