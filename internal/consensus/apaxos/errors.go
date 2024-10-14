6fe139b2aee7f159c84ae34f6ea1612b46d26acf
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-14 19:10:04 -0400

    add: apaxos errors

--- internal/consensus/apaxos/errors.go
+++ internal/consensus/apaxos/errors.go
@@ -3,6 +3,8 @@ package apaxos
 import "errors"
 
 var (
-	errSlowNode       = errors.New("sync received, got a slow node case")
-	errRequestTimeout = errors.New("did not get enough responses")
+	ErrSlowNode         = errors.New("sync received, got a slow node case")
+	ErrNotEnoughServers = errors.New("too many servers are not responding")
+	ErrRequestTimeout   = errors.New("did not get enough responses")
+	ErrNotEnoughBalance = errors.New("not enough balance to process")
 )
