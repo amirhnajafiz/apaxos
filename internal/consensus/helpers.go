1ed23bb0154cdf71fe7646e72d5320400abec6bd
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-15 20:22:36 -0400

    update: helpers

--- internal/consensus/helpers.go
+++ internal/consensus/helpers.go
@@ -9,16 +9,16 @@ import (
 
 // submitTransaction is used to store a transaction into datastore, and update balances.
 func (c Consensus) submitTransaction(transaction *apaxos.Transaction) {
-	c.Logger.Info(
-		"transaction submitted",
-		zap.Int64("amount", transaction.Amount),
-		zap.Int64("seq_number", transaction.SequenceNumber),
-		zap.String("sender", transaction.Sender),
-		zap.String("receiver", transaction.Reciever),
-	)
-
 	// set a unique sequence number for transaction
 	transaction.SequenceNumber = c.Memory.GetSequenceNumber()
+
+	c.Logger.Info(
+		"transaction submitted",
+		zap.Int64("amount", transaction.GetAmount()),
+		zap.Int64("seq_number", transaction.GetSequenceNumber()),
+		zap.String("sender", transaction.GetSender()),
+		zap.String("receiver", transaction.GetReciever()),
+	)
 
 	// make changes into memory for client's balances
 	c.Memory.UpdateBalance(transaction.GetSender(), transaction.GetAmount()*-1)
@@ -46,7 +46,7 @@ func (c Consensus) successfulConsensus(t *apaxos.Transaction) {
 
 // recheck balance checks to see if the client balance has changed or not.
 func (c Consensus) recheckBalance(t *apaxos.Transaction) bool {
-	return c.Memory.GetBalance(c.Client) > t.Amount
+	return c.Memory.GetBalance(c.Client) > t.GetAmount()
 }
 
 // instance exists return true if the apaxos instance is started and running
