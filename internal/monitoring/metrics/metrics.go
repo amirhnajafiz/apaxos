35491805263bb0b738edd3ccb6e861d498ab58da
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-16 00:58:13 -0400

    fix: metrics calculation error

--- internal/monitoring/metrics/metrics.go
+++ internal/monitoring/metrics/metrics.go
@@ -4,6 +4,9 @@ package metrics
 type Metrics struct {
 	latency    float64 // time spent for processing a single transaction in milliseconds
 	throughput float64 // transactions per second parameter
+
+	lNumObservations int // to count the number of latency records
+	tNumObservations int // to count the number of throughput records
 }
 
 // NewMetrics returns a new metrics instance.
@@ -14,22 +17,26 @@ func NewMetrics() *Metrics {
 	}
 }
 
-// ObserveLatency sets a new record for latency by averaging two values.
+// ObserveLatency adds a new latency record and updates the average latency.
 func (m *Metrics) ObserveLatency(latency float64) {
-	if m.latency == 0 {
+	if m.tNumObservations == 0 {
 		m.latency = latency
+	} else {
+		// calculate cumulative moving average
+		m.latency = (m.latency*float64(m.tNumObservations) + latency) / float64(m.tNumObservations+1)
 	}
-
-	m.latency = float64((m.latency + latency) / 2)
+	m.tNumObservations++
 }
 
-// ObserveThroughput sets a new record for throughput by averaging two values.
+// ObserveThroughput adds a new throughput record and updates the average throughput.
 func (m *Metrics) ObserveThroughput(throughput float64) {
-	if m.throughput == 0 {
+	if m.lNumObservations == 0 {
 		m.throughput = throughput
+	} else {
+		// calculate cumulative moving average
+		m.throughput = (m.throughput*float64(m.lNumObservations) + throughput) / float64(m.lNumObservations+1)
 	}
-
-	m.throughput = float64((m.throughput + throughput) / 2)
+	m.lNumObservations++
 }
 
 // GetValues is used to export the current metrics. (latency, throughput)
