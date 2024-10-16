b35ec3ebf5f21f501d561cf91cca523db6caf5a4
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-16 00:49:24 -0400

    update: proto gen script

--- scripts/proto_gen.sh
+++ scripts/proto_gen.sh
@@ -4,8 +4,11 @@
 # as protoc input arguments.
 SRC_DIR="proto/"
 DST_DIR="pkg/"
-PROTO="transactions"
+PROTO_FILES=("transactions" "liveness" "apaxos")
 
 # make sure to have protoc installed on your system
-protoc -I=$SRC_DIR --go_out=$DST_DIR $SRC_DIR/$PROTO.proto || echo "protoc not installed on this machine!"
-protoc -I=$SRC_DIR --go-grpc_out=$DST_DIR $SRC_DIR/$PROTO.proto || echo "protoc not installed on this machine!"
+for PROTO in "${PROTO_FILES[@]}"; do
+  echo "generating Go code for $PROTO.proto ..."
+  protoc -I=$SRC_DIR --go_out=$DST_DIR $SRC_DIR/$PROTO.proto || echo "protoc not installed on this machine!"
+  protoc -I=$SRC_DIR --go-grpc_out=$DST_DIR $SRC_DIR/$PROTO.proto || echo "protoc not installed on this machine!"
+done
